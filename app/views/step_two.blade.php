813b4e09fcb315629c59a674bd3edfbf84c5ad93
Pedro Luna <pedro@mexcorpsoluciones.com>
2014-11-15 01:04:10 -0600

    paso 3

--- app/views/step_two.blade.php
+++ app/views/step_two.blade.php
@@ -78,7 +78,7 @@
 
    </div>
    <div class="col-md-6" style="margin-top: 40px;margin-bottom: 40px; padding-left: 0px; margin-right:0px;">
-        <a href="{{ URL::route('step.two') }}">
+        <a href="{{ URL::route('step.three') }}">
         <div class="col-md-8 pull-right"  style="padding-left: 0px; margin-right:-40px;">
            {{ HTML::image('/img/mano-right.png') }}
            <button style="background: url('/img/boton.png');width:217px;height:45px;color: #ffd19a; margin-left: 10px;">
