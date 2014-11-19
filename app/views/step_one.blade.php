793c886d94cd47d8289abfb090c5cf7482b87b2e
Rainheardt <guillermo@vizinova.com>
2014-11-19 15:26:35 -0600

    CHANGES DONE

--- app/views/step_one.blade.php
+++ app/views/step_one.blade.php
@@ -137,7 +137,7 @@
         </div>
         <div class="row" style="background-color: #ffffff; min-height: 50px;">
             <div class="col-md-12" style="padding-top: 5px;">
-                {{ HTML::image('/img/Heineken.png') }}
+                {{ HTML::image(heineken.png) }}
             </div>
         </div>
     </div>
